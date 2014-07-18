07a6e6f98a4f7e92dd15d3d0a27834cc9e50b1fd
Ian Cox <icox@sullivannyc.com>
2014-07-18 14:48:20 -0400

    moved files

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ Pattern-Gallery
 
 Pattern Gallery is a full screen responsive image gallery for tiled patterns.
 
-It uses PHP to dynamically pull images from the 'img' folder and [slick.js](https://github.com/kenwheeler/slick/) to implement the gallery functionality.
+It uses PHP to dynamically pull images from the 'files' folder and [slick.js](https://github.com/kenwheeler/slick/) to implement the gallery functionality.
 
 ## Use
-To use pattern gallery, simply add your images to the 'img' folder.
+To use pattern gallery, simply add your images to the 'files' folder.
 
 ## Demo
 http://iancox.com/pattern
--- index.php
+++ index.php
@@ -22,25 +22,25 @@
 
     <!-- Prevent text size adjustment on orientation change. -->
     <style>html { -webkit-text-size-adjust: 100%; }</style>
-	<link rel="shortcut icon" href="favicons/favicon.ico">
-	<link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="favicons/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="favicons/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="favicons/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="favicons/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="favicons/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="favicons/apple-touch-icon-152x152.png">
-	<link rel="icon" type="image/png" href="favicons/favicon-196x196.png" sizes="196x196">
-	<link rel="icon" type="image/png" href="favicons/favicon-160x160.png" sizes="160x160">
-	<link rel="icon" type="image/png" href="favicons/favicon-96x96.png" sizes="96x96">
-	<link rel="icon" type="image/png" href="favicons/favicon-16x16.png" sizes="16x16">
-	<link rel="icon" type="image/png" href="favicons/favicon-32x32.png" sizes="32x32">
+	<link rel="shortcut icon" href="assets/favicons/favicon.ico">
+	<link rel="apple-touch-icon" sizes="57x57" href="assets/favicons/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/favicons/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/favicons/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="assets/favicons/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="assets/favicons/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="assets/favicons/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/favicons/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="assets/favicons/apple-touch-icon-152x152.png">
+	<link rel="icon" type="image/png" href="assets/favicons/favicon-196x196.png" sizes="196x196">
+	<link rel="icon" type="image/png" href="assets/favicons/favicon-160x160.png" sizes="160x160">
+	<link rel="icon" type="image/png" href="assets/favicons/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="assets/favicons/favicon-16x16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="assets/favicons/favicon-32x32.png" sizes="32x32">
 	<meta name="msapplication-TileColor" content="#da532c">
-	<meta name="msapplication-TileImage" content="favicons/mstile-144x144.png">
-	<meta name="msapplication-config" content="favicons/browserconfig.xml">
+	<meta name="msapplication-TileImage" content="assets/favicons/mstile-144x144.png">
+	<meta name="msapplication-config" content="assets/favicons/browserconfig.xml">
 	<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js" type="text/javascript"></script>
 
 <script type="text/javascript">
@@ -60,7 +60,7 @@
 <div class="slider single-item">
 
 	<?php
-		$dir = "img";
+		$dir = "files";
 		$imgext = array('jpg','jpeg','png','gif','bmp','tiff','svg');	
 		$files = scandir($dir);
 
@@ -69,7 +69,7 @@
 		if(in_array($ext, $imgext)){
 		if($file != '.' && $file != '..'){?>
 	
-		<div class="img-container" style="background: url('img/<?php echo $file ?>') repeat center center;"></div>
+		<div class="img-container" style="background: url('files/<?php echo $file ?>') repeat center center;"></div>
 
 	
 	<?php }}}?>
@@ -80,7 +80,7 @@
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 	<script src="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.0/highlight.min.js"></script>
 	<script type="text/javascript" src="slick/slick.js"></script>
-	<script type="text/javascript" src="js/scripts.js"></script>
+	<script type="text/javascript" src="assets/js/scripts.js"></script>
 
 </body>
 </html>
